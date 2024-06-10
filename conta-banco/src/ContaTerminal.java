96d3b04535b3606d5798a40c4fe2ce2c169d568a
Ana Paula Oliveira Cardim R. Silva <112723583+paulinhacardim@users.noreply.github.com>
2024-06-10 18:45:51 -0300

    Resolução do Requisito proposto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        // TODO: Conhecer e importar a classe Scanner
+        // Exibir as msgs para o nosso usuario
+        // Obter pela scanner os valores digitados no terminal
+        // Exibir a msg conta criada
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitação e leitura dos dados do usuário
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a nova linha 
+        
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o saldo da Conta:");
+        double saldo = scanner.nextDouble();
+        
+        // Exibição da mensagem final
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+                            + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
